b1f2c1f84916eddc15af624e73d73f69dc5c2493
w75729 <furkanuniversity66@gmail.com>
2026-06-10 20:14:13 +0200

    75729 - inicjalizacja projektu antykwariatu

---
+++ README.md
@@ -0,0 +1,29 @@
+# Antykwariat Akgun
+
+Projekt semestralny - prosta aplikacja internetowa do zarzadzania lista ksiazek w antykwariacie.
+
+Autor: Furkan Akgun  
+Numer indeksu: 75729
+
+## Technologie
+
+- HTML
+- CSS
+- JavaScript
+- Firebase Realtime Database
+- GitHub Pages
+
+## Planowane funkcje
+
+- dodawanie ksiazki,
+- zapis ksiazek do Firebase,
+- pobieranie ksiazek z Firebase,
+- wyswietlanie listy ksiazek,
+- wyswietlanie szczegolow ksiazki,
+- usuwanie ksiazki,
+- wyszukiwanie po tytule lub autorze,
+- prosta walidacja formularza.
+
+## Status
+
+Etap 1: utworzono podstawowa strukture projektu i prosty szkielet strony.
--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 // Numer indeksu: 75729 | Furkan Akgun
 
-function zmienMotyw(kolor) {
-    const link = document.getElementById('theme-link');
-    if (link) { link.href = kolor + '.css'; }
-}
-function toggleSekcja() {
-    const sekcja = document.getElementById('sekcja-projekty');
-    if (sekcja) {
-        if (sekcja.style.display === "none") {
-            sekcja.style.display = "block";
-        } else {
-            sekcja.style.display = "none";
-        }
-    }
-}
+const bookForm = document.getElementById("book-form");
+
+bookForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("Dodawanie ksiazek zostanie dodane w nastepnym etapie.");
+});
